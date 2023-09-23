e036b5a32e4e96a0c59d2e3bf7cba6d4afc9e661
sreeni-pando <sreenivaasan@pando.ai>
2023-09-23 15:45:34 +0530

    db sequelize init

---
+++ wallet-service/server/sequelize/migrations/20230923095154-wallet.js
@@ -0,0 +1,44 @@
+/** @type {import('sequelize-cli').Migration} */
+const tableName = 'Wallet';
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  await queryInterface.createTable(tableName, {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    balance: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    updated_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updated_by: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+  });
+  await queryInterface.addIndex(tableName, ['name'], {
+    indexName: 'wallet_unique_index',
+    indicesType: 'UNIQUE',
+  });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
---
+++ wallet-service/server/sequelize/migrations/20230923095216-wallet-transactions.js
@@ -0,0 +1,61 @@
+'use strict';
+const tableName = 'WalletTransactions';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable(tableName, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      wallet_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      transaction_id: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      balance: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      balance: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_by: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+    await queryInterface.addIndex(tableName, ['wallet_id', 'transaction_id' ], {
+      indexName: 'wallet_transactions_unique_index',
+      indicesType: 'UNIQUE',
+    });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
