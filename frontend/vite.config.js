274393cc1fd848194d6ea1eeacda468adc6e2c7f
sreeni-pando <sreenivaasan@pando.ai>
2023-09-27 22:36:13 +0530

    Frontend dist changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,19 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vitePluginRequire from 'vite-plugin-require';
 
 export default defineConfig({
-  base: '/',
-
   plugins: [vue()],
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-    dedupe: [
-      'vue',
-    ],
   },
   server: {
     port: 8080,
   },
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/main.js'),
-      name: 'MyLib',
-      fileName: (format) => `my-lib.${format}.js`,
-    },
-    outDir: './dist/',
+    outDir: './dist',
   },
+  base: '/',
 });
