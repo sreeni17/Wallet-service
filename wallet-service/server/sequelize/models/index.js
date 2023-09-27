79e955e9f073f672dbe8882e0fe2de01ee14d6ab
sreeni-pando <sreenivaasan@pando.ai>
2023-09-27 21:23:03 +0530

    db config get

--- wallet-service/server/sequelize/models/index.js
+++ wallet-service/server/sequelize/models/index.js
@@ -5,8 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/database.js')[env];
+const envConfig = require('../../../env-config/index');
+const config = envConfig.get('wallet-db');
 const db = {};
 config.define = {
   underscored: true,
