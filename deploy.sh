bd7e4cbe4e0bba401df69147d7a93910bb6af2a2
sreeni-pando <sreenivaasan@pando.ai>
2023-09-28 08:13:51 +0530

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Copy the config from cloud
-# echo "Copy the config from cloud..."
-# aws s3 cp s3://wallet-highlevel ./config --recursive
-# echo "copied"
+Copy the config from cloud
+echo "Copy the config from cloud..."
+aws s3 cp s3://wallet-highlevel ./config --recursive
+echo "copied"
 
 echo "Copy the nginx to machine"
 cp config/nginx.conf /etc/nginx/nginx.conf
@@ -13,15 +13,15 @@ echo "restart nginx"
 systemctl restart nginx
 echo "restarted"
 
-echo "Make bootstrap"
-make bootstrap
-echo "bootstrap completed"
+# echo "Make bootstrap"
+# make bootstrap
+# echo "bootstrap completed"
 
-echo "start frontend build"
-cd frontend/
-yarn build
-cd ../
-echo "build completed"
+# echo "start frontend build"
+# cd frontend/
+# yarn build
+# cd ../
+# echo "build completed"
 
 
 # Run Bootstrap
