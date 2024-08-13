5acf4a50a1a58081037f02726814849108abbc8a
sreeni17 <sreeni08.g+1@gmail.com>
2024-08-13 18:52:20 +0530

    revert url

--- frontend/axios.js
+++ frontend/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const hostname = window.location.host;
 const isDevelopment = hostname.startsWith('localhost') || hostname.startsWith('127.0.0.1:8080');
-const baseURL = isDevelopment ? 'https://kube-genai-agent-creator.service.intradsm1.sdeconsul.csnzoo.com' : 'https://kube-genai-agent-creator.service.intradsm1.sdeconsul.csnzoo.com';
+const baseURL = isDevelopment ? 'http://localhost:3000/api' : '/api';
 
 const axiosInstance = axios.create({ baseURL, timeout: 6000 });
 
